12e280f34d71b90f1ff96d0ae9227c4f55fb2dbb
Patxinho <santi.r.ortego@gmail.com>
2014-10-01 18:02:24 +0200

    cambiando

--- server.js
+++ server.js
@@ -12,13 +12,6 @@ app.get('/', function (req, res) {
     res.send("Hello world!");
 });
 
-routes = require('./controllers/tvshows')(app);
-
-// Connection to DB
-mongoose.connect('mongodb://MongoLab-wy:wNGd2KKc.KUMZv4V185dh6MQ8nH7rO1yzsiC4iaGabY-@ds052827.mongolab.com:52827/MongoLab-wy', function (err, res) {
-    if (err) throw err;
-    console.log('Connected to Database');
-});
 
 
 app.listen(3000, function () {
